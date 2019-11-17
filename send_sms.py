28fb36b110e5d702dedf348da14718e523d9d57e
7daylenrobertson <9daylenrobertson@gmail.com>
2019-11-17 10:36:28 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,5 +1,6 @@
 from twilio.rest import Client
 import geocoder
+import cv2
 # You had to sign up with a twilio account in order to use the api...
 #In your code. So that's what we did in line 6-7. This is our trial account
 account_sid = "AC34a5d68ec0d37b601dce607161387a0a"
@@ -21,13 +22,29 @@ sos = client.messages.create(
     body= ("I AM BEING HELD AGAINST MY WILL."+" MY NAME IS " +userName.upper()+ (". MY LATITUDE IS: " +str(g.lat)) + (". MY LONGITUDE: " +str(g.lng))+ (". MY CITY AND STATE IS: " +str(g))))
 
 print(sos.sid)
-while counter<=30:
+while counter<=5001:
  counter+=1
- if counter>25:
+ if counter>5000:
     g = geocoder.ip('me')
     print(sos.sid)
 
+#Code to control camera (in order to send out pictures of surroundings every 5 minutes. Has not been applied to main code)
+#Must install (pip install numpy) (pip install opencv-python) for cv2 to work
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
 
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
 #This can be used to send out to family members when 911 is texted
 #numbers = ['+number', '+number', '+number']
 #for number in numbers:
